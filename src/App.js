d04be7fb95a5080d2acb58a9816545aa4192d432
DanielAros <danielaros200@gmail.com>
2022-03-15 08:26:40 -0600

    Tarea semana 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import Pokemon from './components/Pokemon.jsx';
+import {Button} from 'react-bootstrap';
+import Pokemons from './components/Pokemons.json';
+import { useState } from 'react';
+import styles from "./components/App.module.css";
 
 function App() {
+
+  const [pokemon, setPokemon] = useState(Pokemons);
+ 
+  const agregarPokemon = () => {
+    const nuevoPokemon = {}
+    nuevoPokemon.name = prompt("Ingresa el nombre del pokemon: ");
+    nuevoPokemon.base_experience = prompt("Ingresa la experiencia: ");
+    setPokemon((pokemon) => [...pokemon, nuevoPokemon]);
+    alert("numero total de pokemons: " + (pokemon.length+1));
+  }
+
   return (
-    <Pokemon name="picachu"/>
+    <div className={`${styles.container}`}>
+      <header>
+        <h1>POKEDEX</h1>
+      </header>
+      <div className={`${styles.cards}`}>
+        {
+          pokemon.map((item) => <Pokemon name={item.name} base_experience={item.base_experience}/>)
+        }
+      </div>
+      <Button variant="primary" onClick={agregarPokemon}>Agregar Pokemon</Button>
+    </div>
   );
 }
 
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
+import {Card, Button} from 'react-bootstrap';
+import styles from "./Pokemon.module.css"
 
 function Pokemon(props){
 
-    const [name, setName] = useState(props.name);
+    const [pokemon, setPokemon] = useState(props);
     
     return (
-        <>
-            <p>
-                El nombre del pokemon es: <b>{name}</b>
-            </p>
-            <button onClick={() => setName('charmander')}>Cambiar Nombre</button>
-        </>
+        <Card className={`${styles.card}`}>
+            <Card.Body>
+                <Card.Title>Pokemon</Card.Title>
+                <Card.Text>
+                    <b>Name:</b> {props.name}
+                </Card.Text>
+                <Card.Text>
+                    <b>Base experience:</b> {props.base_experience}
+                </Card.Text>
+            </Card.Body>
+        </Card>
     );
 }
 
